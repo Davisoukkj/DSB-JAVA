7f56c62f8012f7c5460020f7af93cc3f3942968c
Davisoukkj <130378822+Davisoukkj@users.noreply.github.com>
2023-09-04 11:44:54 -0300

    Add files via upload
    novas pastas em java adicionadas

---
+++ 2dsb js/introducao-javascript/js/principal.js
@@ -0,0 +1,9 @@
+//------------------------------------------------
+//daqui pra baixo começa o js do site
+
+//busca no site pelo titulo e armazena ele dentro da variavel "titulo"
+let titulo = (document.querySelector(".titulo"));
+//mostra no console o texto que esta dentro da variavel "titulo"
+console.log(titulo.textContent);
+//altera o conteudo da variavel "titulo" para o texto desejado
+titulo.textContent = "Fica Grande Nutrição"
