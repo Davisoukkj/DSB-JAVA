735b372c2ba793c6ae5c3b4a10242b9e8beebc43
Davisoukkj <130378822+Davisoukkj@users.noreply.github.com>
2023-09-06 12:29:41 -0300

    Delete 2dsb js directory

--- 2dsb js/introducao-javascript/js/principal.js
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------
-//daqui pra baixo começa o js do site
-
-//busca no site pelo titulo e armazena ele dentro da variavel "titulo"
-let titulo = (document.querySelector(".titulo"));
-//mostra no console o texto que esta dentro da variavel "titulo"
-console.log(titulo.textContent);
-//altera o conteudo da variavel "titulo" para o texto desejado
-titulo.textContent = "Fica Grande Nutrição"
