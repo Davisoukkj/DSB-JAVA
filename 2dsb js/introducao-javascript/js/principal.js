299398c68d56325976387e89e2dd8e81a5bd90e1
Davisoukkj <130378822+Davisoukkj@users.noreply.github.com>
2023-09-20 12:28:45 -0300

    Add files via upload

--- 2dsb js/introducao-javascript/js/principal.js
+++ 2dsb js/introducao-javascript/js/principal.js
@@ -10,32 +10,47 @@ console.log(titulo.textContent);
 //altera o conteudo da variavel "titulo" para o texto desejado
 titulo.textContent = "Fica Grande Nutrição"
 
-//mudanças na tabela
-//armazena na variavel paciente os dados do primeiro-paciente
-let paciente = document.querySelector("#primeiro-paciente")
-//mostra no console o valor da variavel "paciente"
-console.log(paciente);
+//mudanças na tabela 
 
+//armazena no array paciente os dados do primeiro-paciente
+let pacientes = document.querySelectorAll(".paciente");
+
+//criação do looping for
+for(i = 0; i < 5;  i++){
+
+let paciente = pacientes [i];
 let tdPeso = paciente.querySelector(".info-peso");
 let tdAltura = paciente.querySelector(".info-altura");
 
-
 let peso = tdPeso.textContent;
 let altura = tdAltura.textContent;
+
 
 let imc = peso / (altura*altura);
 console.log(imc);
 
 let tdImc = paciente.querySelector(".info-imc");
-tdImc.textContent = imc;
 
 //validações de dados
-if (peso <= 0 || peso >= 1000) {
-    tdImc.textContent = "peso invalido"
+let pesoEhValido = true;
+let alturaEhValida = true;
+
+//verifica se o peso é valido
+if(peso <= 0 || peso >= 1000) {
+    tdImc.textContent = "Peso inválido!";
+    pesoEhvalido = false
 }
-if (altura <= 0 || altura >= 3) {
-    tdImc.textContent = "Altura invalido"
+//verifica se a altura é valida
+if(altura <=0 || altura >=3) {
+    tdImc.textContent = "Altura inválido!";
+    alturaEhValida = false;
 }
-//DESAFIO!!
-//FAZER APARECER O ERRO "PESO INVALIDO"
-//NA CÉLULA DO IMC NA TABELA
+//verifica se o peso e a altura são validos
+if(pesoEhValido && alturaEhValida){
+    let imc = peso / (altura*altura);
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = "Altura ou peso incorretos!"
+}  
+}
+
